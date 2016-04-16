a51c183a78babe20685160e1ec84015ff10952f3
Thanos Korakas <thanoskorakas@gmail.com>
2016-04-16 14:07:39 +0300

    Some fixes.

--- server/app/js/services/socket.srv.js
+++ server/app/js/services/socket.srv.js
@@ -1,5 +1,7 @@
+'use strict';
+
 angular.module('chatApp').factory('socket', function ($rootScope) {
-    var socket = io();
+    var socket = io('localhost:3000');
     return {
         on: function (eventName, callback) {
             socket.on(eventName, function () {
